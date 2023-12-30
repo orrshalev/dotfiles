4721eeaca8b196f90d4fb5cac0007a3a026df727
Orr Shalev <shalev.orr@gmail.com>
2023-12-29 23:50:09 -0500

    Removed minor thing from zsh

--- zsh/.config/zsh/envvars.zsh
+++ zsh/.config/zsh/envvars.zsh
@@ -15,6 +15,3 @@ elif command -v vim >/dev/null 2>&1; then
     export EDITOR='vim'
 fi
 export VISUAL="emacsclient -c -a ''"
-
-# likely to remove
-# export JAVA_HOME="/usr/lib/jvm/java-21-openjdk-21.0.0.0.35-1.rolling.fc38.x86_64"
