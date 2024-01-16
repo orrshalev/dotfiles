a2a4f8494cddfc05a367c009c7c43c62ef692d73
Orr Shalev <shalev.orr@gmail.com>
2024-01-16 11:25:32 -0500

    Minor changes to zsh

--- zsh/.config/zsh/platform-dependent.zsh
+++ zsh/.config/zsh/platform-dependent.zsh
@@ -5,9 +5,12 @@ Darwin)
 	# echo 'Mac OS X'
   defaults write -g InitialKeyRepeat -int 13 # normal minimum is 15 (225 ms)
   defaults write -g KeyRepeat -int 2 # normal minimum is 2 (30 ms)
+  export PATH=$HOME/.emacs.d/bin:$PATH
+  alias upgrade="brew upgrade"
 	;;
 Linux)
 	# echo 'Linux'
+  alias upgrade="sudo dnf upgrade --refresh"
 	;;
 CYGWIN* | MINGW32* | MSYS* | MINGW*)
 	# echo 'MS Windows'
--- zsh/.config/zsh/shortcuts.zsh
+++ zsh/.config/zsh/shortcuts.zsh
@@ -7,7 +7,6 @@ alias fservorr="ssh -p 7777 orrshalev@fservorr.xyz"
 alias cf="cd ~/dotfiles"
 alias lzyj="cd ~/GoogleDrive/MProject"
 
-alias upgrade="sudo dnf upgrade --refresh"
 alias clip="xclip -selection clipboard"
 
 alias ke="pgrep -f 'emacs --daemon' | xargs -r kill"
