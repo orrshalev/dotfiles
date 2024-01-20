4dbb183ddb0a5aa46ecda59ef9d9f82aef445f79
Orr Shalev <shalev.orr@gmail.com>
2024-01-20 17:09:48 -0500

    ...

--- emacs/.config/doom/config.el
+++ emacs/.config/doom/config.el
@@ -84,8 +84,13 @@
   :config
   (custom-set-faces
    '(org-level-1 ((t (:inherit outline-1 :height 1.5))))
-   '(org-level-2 ((t (:inherit outline-2 :height 1.4))))
-   '(org-level-3 ((t (:inherit outline-3 :height 1.3))))
+   '(org-level-2 ((t (:inherit outline-2 :height 1.2))))
+   '(org-level-3 ((t (:inherit outline-3 :height 1.1))))
    '(org-level-4 ((t (:inherit outline-4 :height 1.0))))
    '(org-level-5 ((t (:inherit outline-5 :height 1.0)))))
 )
+
+(setq
+ doom-font (font-spec :family "CaskaydiaCove Nerd Font" :size 13)
+ doom-variable-pitch-font (font-spec :family "CaskaydiaCove Nerd Font")
+ global-prettify-symbols-mode t)
