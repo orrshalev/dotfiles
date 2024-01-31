d050423cb834c89fe3b8d27788edd829c8b83de0
Orr Shalev <shalev.orr@gmail.com>
2024-01-31 15:35:39 -0500

    null

--- zsh/.config/zsh/functions.zsh
+++ zsh/.config/zsh/functions.zsh
@@ -4,3 +4,17 @@
 function sp() {
     ps aux | grep $1
 }
+
+function le() {
+    emacsclient -c -a "" &
+    # Capture the PID of the last backgrounded job
+    job_pid=$!
+    # Wait for a brief moment to allow the command to fail or succeed
+    sleep 0.05
+    # Check if the emacsclient is still running
+    if kill -0 $job_pid > /dev/null 2>&1; then
+        disown
+    else
+        echo "Failed to start emacsclient."
+    fi
+}
